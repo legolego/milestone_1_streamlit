a1bb8dbf3f9292cc34c57a9e9faa2c0af98f3752
corsilt <corsilt1230@hotmail.com>
2021-05-15 14:38:04 -0400

    second commit

--- streamlit/milestone_app.py
+++ streamlit/milestone_app.py
@@ -2,48 +2,53 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+# https://drive.google.com/drive/folders/13cpp-BNdP6Sc623PyR8_Fq0YzwK6lZ1Z?usp=sharing
+
+# url = ' google drive sharing link'
+# path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+
 st.title("Predict New Imagekjhkjhs")
 st.header("test html import")
 
-HtmlFile = open(r"streamlit\data\total_pickups.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\total_pickups.html", 'r')
 source_code = HtmlFile.read() 
 #print(source_code)
 components.html(source_code,  height = 600, width=700,)
 
-HtmlFile = open(r"streamlit\data\dropoffs.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\dropoffs.html", 'r')
 source_code = HtmlFile.read() 
 #print(source_code)
 components.html(source_code,  height = 600, width=700,)
 
 st.title("Predict New Imagekjhkjhs")
-HtmlFile = open(r"streamlit\data\credit_card_tips.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\credit_card_tips.html", 'r')
 source_code = HtmlFile.read() 
 components.html(source_code,  height = 600, width=700,)
 
 st.title("Predict New Imagekjhkjhs")
-HtmlFile = open(r"streamlit\data\cash_tips.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\cash_tips.html", 'r')
 source_code = HtmlFile.read() 
 components.html(source_code,  height = 600, width=700,)
 
 st.title("Predict New Imagekjhkjhs")
-HtmlFile = open(r"streamlit\data\specific_day.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\specific_day.html", 'r')
 source_code = HtmlFile.read() 
 components.html(source_code,  height = 600, width=700,)
 
 st.title("Predict New Imagekjhkjhs")
-HtmlFile = open(r"streamlit\data\single_pass.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\single_pass.html", 'r')
 source_code = HtmlFile.read() 
 components.html(source_code,  height = 600, width=700,)
 
 st.title("Predict New Imagekjhkjhs")
-HtmlFile = open(r"streamlit\data\double_pass.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\double_pass.html", 'r')
 source_code = HtmlFile.read() 
 components.html(source_code,  height = 600, width=700,)
 
 st.title(" ")
 st.title(" ")
 st.title("Predict New Imagekjhkjhs")
-HtmlFile = open(r"streamlit\data\multi_pass.html", 'r')
+HtmlFile = open(r"milestone_1_streamlit\streamlit\data\multi_pass.html", 'r')
 source_code = HtmlFile.read() 
 components.html(source_code,  height = 600, width=700,)
 
