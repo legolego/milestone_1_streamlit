efc2012287f5bfff07137351fc7b7799d5f9b197
legolego <oleg_gitgub@olegs.com>
2021-05-16 23:54:33 -0400

    added a few more charts to streamlit

--- streamlit/milestone_app.py
+++ streamlit/milestone_app.py
@@ -1,10 +1,12 @@
-
+import os
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import altair as alt
 import numpy as np
 import statsmodels.formula.api as smf
+from datetime import timedelta
+import datetime as dt
 
 alt.data_transformers.disable_max_rows()  
 # to run this:
@@ -21,20 +23,19 @@ st.info(
         ([Source](https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page)) ")
 
 
-    
-
 # https://docs.streamlit.io/en/0.65.0/advanced_concepts.html
-df_taxi_sample = pd.read_csv("streamlit/data/hourly-2020-sample.csv")
+df_taxi_sample = pd.read_csv("streamlit/data/yellow_tripdata_2010_sample.csv")
 st.dataframe(df_taxi_sample)
 
 st.info(
     "Next is the hourly weather data we used. This is from NOAA, using Central Park as the collection point.\
         To get it, go ([NOAA](https://www.ncdc.noaa.gov/data-access))  --> Data Access --> Quick Links --> US Local -->\
              Local Climatological Data (LCD) --> Choose your location(Add to Cart) --> Go to cart at top --> \
-                 LCD CSV, date range --> Continue and give them your email, they'll send it quickly. ")
+                 LCD CSV, date range --> Continue and give them your email, they'll send it quickly. The documentation is \
+                     [here](https://www1.ncdc.noaa.gov/pub/data/cdo/documentation/LCD_documentation.pdf). ")
 
 # https://docs.streamlit.io/en/0.65.0/advanced_concepts.html
-df_taxi_sample = pd.read_csv("streamlit/data/yellow_tripdata_2010_sample.csv")
+df_taxi_sample = pd.read_csv("streamlit/data/hourly-2020-sample.csv")
 st.dataframe(df_taxi_sample)
 
 
@@ -129,7 +130,7 @@ df_ols = df_reg
 with st.echo():
     reg = smf.ols(formula = 'RidesCount ~ Tuesday + Wednesday + Thursday + Friday + Saturday + Sunday +  HourlyDryBulbTemperature + treatment + afternoon_dummy + treatment:afternoon_dummy', data = df_ols).fit()
     ols_robust2_1 = reg.get_robustcov_results(cov_type= 'HC1') 
-    st.write(ols_robust2_1.summary())
+    ols_robust2_1.summary()
 
 df_coeffs = pd.DataFrame(reg.params).reset_index()
 df_95errs = pd.DataFrame(ols_robust2_1.conf_int())
@@ -171,7 +172,35 @@ st.altair_chart((error_bars + points + rule).interactive())
 
 
 
+df_single = pd.read_csv('streamlit/data/single20100117.csv')
+df_single['b_ts'] = pd.to_datetime(df_single['b_ts'], format='%Y-%m-%d %H:%M:%S')
+df_single['pickup_datetime_1min'] = pd.to_datetime(df_single['pickup_datetime_1min'], format='%Y-%m-%d %H:%M:%S')
+df_single['pickup_datetime_same_time'] = pd.to_datetime(df_single['pickup_datetime_same_time'], format='%Y-%m-%d %H:%M:%S')
+
+b = pd.to_datetime(df_single['b_ts'].iloc[0])
 
 
+line = alt.Chart(pd.DataFrame({'x': [str(b)]})).mark_rule().encode(x=alt.X('x:T', title='') )    
 
+# https://altair-viz.github.io/user_guide/times_and_dates.html
+rides = alt.Chart(df_single, title='B_ID: ' + str(df_single.iloc[0]['b_id']) + " - Rides around " + str(b.strftime('%A')) + ' - ' + str(b)).mark_line().encode(
+    #x=alt.X('hoursminutes(pickup_datetime_1min):Q', title='Date/time', 
+    #x=alt.X('utchoursminutes(pickup_datetime_1min_str):T', title='Date/time', 
+    x=alt.X('pickup_datetime_same_time:T', title='Date/time', 
+            axis=alt.Axis(
+                        tickCount=18,
+                    )
+            ), 
+    #x=alt.X('sequence:Q', axis=None, title='Date/time' ),     
+    y=alt.Y('rides_per_minute:Q', title='Rides per minute'),
+    color=alt.Color('which_week:N', title='Which week', scale=alt.Scale(
+                                            domain=['Week before', 'Current week', 'Week after'],
+                                            range=['#DB9EA6 ', '#FFE303', '#adc0d8']))
+).properties(
+width=700,
+    height=250
+).interactive()
+
+
+st.altair_chart(rides+line)
 
